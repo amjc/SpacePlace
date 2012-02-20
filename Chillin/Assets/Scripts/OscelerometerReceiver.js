4400cf05214564f91e7e983093958200daa3796c
Steven Klise <steven@stevenklise.com>
2012-02-20 14:41:30 -0500

    Arduino controls thrust brake and camera look

--- Chillin/Assets/Scripts/OscelerometerReceiver.js
+++ Chillin/Assets/Scripts/OscelerometerReceiver.js
@@ -1,61 +1,83 @@
-var zeroAc: Vector3;
-
-var start = false;
-
-public var scaling : float = 1;
+public var acceleration : float = 1.0;
 public var breakingPower : float = 0.333333333;
-
+public var maxSpeed : float = 200;
+public var torqueCoefficient : float = 40;
+public var torqueSensitivity: float = 1;
 
 // Readings from accelerometer
 private var accelerometer : Vector3;
 private var previousAccelerometer : Vector3;
 private var trailingReadings : Vector3[];
 private var trailingIndex : int = 0;
-private var trailingLength : int = 10;
+private var trailingLength : int = 30;
 private var trailingAvg : Vector3;
 
-var AccelerometerUpdateInterval : float = 1.0 / 60.0;
-var LowPassKernelWidthInSeconds : float = 1.0;
+private var AccelerometerUpdateInterval : float = 1.0 / 120.0;
+private var LowPassKernelWidthInSeconds : float = 1.5;
 
 private var LowPassFilterFactor : float = AccelerometerUpdateInterval / LowPassKernelWidthInSeconds; // tweakable
 private var lowPassValue : Vector3 = Vector3.zero;
 
 function Start () {
-	accelerometer = Vector3.zero;
-	previousAccelerometer = Vector3.zero;
-	lowPassValue = Vector3.zero;
-	trailingReadings = [Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero];
+  accelerometer = Vector3.zero;
+  previousAccelerometer = Vector3.zero;
+  lowPassValue = Vector3.zero;
+  trailingReadings = [Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero];
 }
 
+function LowPassFilterAccelerometer(accelerometer : Vector3) : Vector3 {
+  lowPassValue = Vector3.Lerp(lowPassValue, trailingAvg, LowPassFilterFactor);
+  return lowPassValue;
+}
 
-function LowPassFilterAccelerometer() : Vector3 {
-	lowPassValue = Vector3.Lerp(lowPassValue, trailingAvg, LowPassFilterFactor);
-	return lowPassValue;
+function trailingAverage() {
+  var trailingSum = Vector3.zero;
+  for( i = 0; i < trailingLength; i++) {
+    trailingSum += trailingReadings[i];
+  }
+  return trailingSum / trailingLength;
 }
 
 function Update () {
-	var trailingSum = Vector3.zero;
-	for( i = 0; i < trailingLength; i++) {
-		trailingSum += trailingReadings[i];
-	}
-	trailingAvg = trailingSum / 10.0;
-	Debug.Log(trailingAvg);
-	var newSpot = LowPassFilterAccelerometer();
-  // rigidbody.AddRelativeForce(newSpot * scaling);
+  var averageAccelerometer = trailingAverage();
+
+  // var eulerAngleVelocity : Vector3 = torqueCoefficient * LowPassFilterAccelerometer(averageAccelerometer);
+
+  if(averageAccelerometer.magnitude >= torqueSensitivity) {
+    var torque = averageAccelerometer * torqueCoefficient;
+    // var deltaRotation : Quaternion = Quaternion.Euler(eulerAngleVelocity * Time.deltaTime);
+    // rigidbody.MoveRotation(rigidbody.rotation * deltaRotation);
+    rigidbody.AddRelativeTorque(torque);
+  }
+
+  // Clamp speed
+  rigidbody.velocity = Vector3.ClampMagnitude(rigidbody.velocity, maxSpeed);
+
+  // Debug.Log("V=" + rigidbody.velocity + " D=" + rigidbody.rotation);
 }
 
 function OSCMessageReceived(message : OSC.NET.OSCMessage){
-//	Debug.Log("Message Received with address " + message.Address + " with " + message.Values[0] + " values");
-	accelerometer = new Vector3(-1 * message.Values[0], -1 * message.Values[2], -1 *message.Values[1]);
-	trailingReadings[trailingIndex] = accelerometer - previousAccelerometer;
+  if(message.Address == "/read/accelerometer") {
+    receiveAccelerometer(message);
+  }
+}
 
-	if(message.Values[3] == 1) {
-		rigidbody.drag = breakingPower;
-	} else {
-	  rigidbody.drag = 0;
-	}
+// When the OSC message is from the accelerometer, do this.
+function receiveAccelerometer(message : OSC.NET.OSCMessage) {
+  accelerometer = new Vector3(-1 * message.Values[0], -1 * message.Values[2], -1 *message.Values[1]);
+  trailingReadings[trailingIndex] = accelerometer - previousAccelerometer;
 
-	previousAccelerometer = accelerometer;
-	trailingIndex = (trailingIndex + 1) % trailingLength;
+  // Turn on the brake
+  if(message.Values[3] == 1) {
+    rigidbody.drag = breakingPower;
+  } else {
+    rigidbody.drag = 0;
+  }
+  // Turn on the thruster
+  if(message.Values[4] == 1) {
+    rigidbody.AddRelativeForce(Vector3.forward * acceleration);
+  }
 
+  previousAccelerometer = accelerometer;
+  trailingIndex = (trailingIndex + 1) % trailingLength;
 }
